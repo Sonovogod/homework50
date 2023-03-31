ea898bdce1160ab730ab6c266b1600942e4f17c7
Nikita <nikita.dronoff@gmail.com>
2023-04-01 02:34:04 +0600

    - обновил метод вывода и ввода картинки для SeaFood.cs (убран хардкод)

---
+++ HomeWork/Migrations/20230331203159_added image prop in to the SeaFood.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeWork.Migrations
+{
+    public partial class addedimagepropintotheSeaFood : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "SeaFoods",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "SeaFoods");
+        }
+    }
+}
--- HomeWork/Models/SeaFood.cs
+++ HomeWork/Models/SeaFood.cs
@@ -5,6 +5,7 @@ public class SeaFood
     public int Id { get; set; }
     public string Title { get; set; }
     public string Discription { get; set; }
+    public string Image { get; set; }
     public decimal Weight { get; set; }
     public DateTime ExpirationDate { get; set; }
     public int Coast { get; set; }
